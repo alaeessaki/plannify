7bbf00ce20ce95bdb2cad7a6aa6e2882694d5395
Alae Es-saki <alaeessaki@gmail.com>
2019-11-26 11:21:29 +0100

    adding disconnect

--- plannify/src/models/Connexion.java
+++ plannify/src/models/Connexion.java
@@ -5,30 +5,46 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connexion {
-private static Connection maconex  ;
+	
+	private static Connection connection;
+	private static String dbname = "suivie_des_bugs";
 	
 	public static Connection getConx() {
-
-		try {
-		Class.forName("com.mysql.jdbc.Driver");
-		System.out.println("Driver O.K.");
-		String url ="jdbc:mysql://localhost:3306/suivie_des_bugs";
-		String user="root";
-		String passwd="";
-		maconex = DriverManager.getConnection(url,user,passwd);
-		System.out.println("Connexion effective !");
+		if(connection!=null) {
+			return connection;
+		}else {
+			try {
+			Class.forName("com.mysql.jdbc.Driver");			
+			String url ="jdbc:mysql://localhost:3306/"+dbname+"??useSSL=false";
+			String user="root";
+			String passwd="";
+			
+			connection = DriverManager.getConnection(url,user,passwd);
+			
+			}catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				//connection
+			return connection;
+			
 		}
-	 catch (ClassNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
 	}
-		
-		//connection
-		return maconex;
+	
+	//Disconnect
+	public static void disconnect() {
+		if(connection==null) {
+			return;
+		}
+		try {
+			connection.close();
+			connection=null;
+		}catch(Exception e) {	
+			e.printStackTrace();
+		}
 	}
-
-
+	
 }
