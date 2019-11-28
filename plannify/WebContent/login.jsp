a4a6f9fd5c158e78aa0f7b852c4d183dfd1a3c23
KhalilKes <akakhalilyo@gmail.com>
2019-11-28 20:36:17 +0100

    login and signup front

--- plannify/WebContent/login.jsp
+++ plannify/WebContent/login.jsp
@@ -2,11 +2,36 @@
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
+    <meta charset="ISO-8859-1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/login.css">
+    <title>login</title>
 </head>
+
 <body>
+    <div class="container-fluid box-2 d-flex justify-content-center align-items-center">
+        <div class="card-login d-flex justify-content-center align-items-center">
+            <form action="login" method="post">
+                <div class="form-group">
+                    <input type="email" class="input-style" name="email" id="" aria-describedby="helpId"
+                        placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="input-style" name="password" id="" aria-describedby="helpId"
+                        placeholder="Password">
+                </div>
+                <button type="submit" class="btn-login btn-block">LOGIN</button>
+                <div class="d-flex mt-3">
+                    <p>not registered yet ? </p> <a href="#" class="login-redirect ml-2">create account</a>
+                </div>
+            </form>
+        </div>
+    </div>
 
 </body>
+
 </html>
--- plannify/src/controllers/SignupServlet.java
+++ plannify/src/controllers/SignupServlet.java
@@ -1,29 +1,26 @@
 package controllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import beans.User;
 import models.UserDAO;
 
 /**
- * Servlet implementation class AuthServlet
+ * Servlet implementation class SignupServlet
  */
-@WebServlet("/login")
-public class AuthServlet extends HttpServlet {
+@WebServlet("/SignupServlet")
+public class SignupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AuthServlet() {
+    public SignupServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,35 +29,21 @@ public class AuthServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		PrintWriter out = response.getWriter();
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		
-		
-		out.println("testing the parameters  " + email + " " + password);
-		
 		try {
-			UserDAO udao = new UserDAO();		
-			User login = udao.getUser(email, password);
 			
-			if(login != null) {
-				HttpSession session = request.getSession();
-				session.setAttribute("SessionUser", login);
-				
-				out.println("<h1> welcome "+ login.getNom() +" </h1>");
-			
-			}else {
-				out.println("<h1> Login Error </h1>");
-				
-			}			
 			
 		}catch(Exception e) {
 			e.printStackTrace();
